d6c416d43975c702a3d76de9ab494056ec83247e
kestralbialima <biancal399@gmail.com>
2026-04-08 15:31:57 -0300

    chore: setup github actions deploy 2026

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
-  base: '/sistema-custom/', // 👈 ADICIONE ISSO (use o nome exato do seu repo no GitHub)
+  base: '/sistema-custom/', // Garanta que este seja o nome do seu repositório
 })
